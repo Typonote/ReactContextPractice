9a2eae43a84c2d908101d7e42f7af355e564961c
JinKyung Kim <kimradium@naver.com>
2021-11-20 21:22:35 +0900

    #4 useContext로 관리하기

--- src/Components/ColorBox.js
+++ src/Components/ColorBox.js
@@ -2,29 +2,27 @@ import React, { useContext } from "react";
 import ColorContext, { ColorConsumer } from "../Contexts/Color";
 
 // props가 아닌 Consumer를 이용하여 색상을 받아옴
+
+// useContext를 사용하면 context를 편하게 사용할 수 있다.
 const ColorBox = () => {
   const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 };
 
